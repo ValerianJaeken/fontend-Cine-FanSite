1443253959db6dfce39dd1aef029e48b6edc60af
dayvidastua <dayvidastua@gmail.com>
2018-02-07 15:44:55 +0100

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 
 
@@ -38,10 +38,9 @@
 
 $(".single-item").slick({
 	dots: true
-=======
 $(document).ready(function(){
   $('.headerCar').slick({
     
   });
->>>>>>> 665226dba1d9cbae638562c3ed37aeb48cbbc5b6
 });
+}
