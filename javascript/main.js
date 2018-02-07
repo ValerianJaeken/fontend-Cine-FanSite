20d9562d4fe64b191bc01ef444f23308642a95ac
ValerianJaeken <valerianrj@gmail.com>
2018-02-07 15:18:44 +0100

    CAROUSEL YEEEEES
    CAROUSEL YEEEEES

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,5 @@
 $(document).ready(function () {
   $('.headerCar').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-    dots: true,
-    draggable: false,
-    fade: true,
-    speed: 1000
-  });
 
-  $('.headerCar').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    $(slick.$slides).removeClass('is-animating');
-  });
-
-  $('.headerCar').on('afterChange', function(event, slick, currentSlide, nextSlide) {
-    $(slick.$slides.get(currentSlide)).addClass('is-animating');
   });
 });
